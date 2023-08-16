7d6f602deb3c4b4fd407965b69a97e320d6f2443
janardanaryal <125633317+janardanaryal@users.noreply.github.com>
2023-08-16 12:10:30 -0700

    Update preeti_to_unicode.js

--- assets/js/preeti_to_unicode.js
+++ assets/js/preeti_to_unicode.js
@@ -1,6 +1,6 @@
 function unicodeConverter()
-{var array_one=new Array("Ã§","Ëœ",".","'m","]m","Fmf","Fm",")","!","@","#","$","%","^","&","*","(","k|m","em","km","Qm","qm","NËœ","Â¡","Â¢","1","2","4",">","?","B","I","Q","ÃŸ","q","â€ž","â€¹","â€¢","â€º","Â§","Â°","Â¶","Â¿","Ã…","Ã‹","ÃŒ","Ã","ÃŽ","Ã","Ã¥","6Â«","7Â«","8Â«","9Â«","Ã˜","|","8Ãž","9Ãž","S","s","V","v","U","u","Â£","3","Âª","R","r","5","H","h","â€°","Â´","~","`","6","7","8","9","0","T","t","Y","y","b","W","w","G","g","K","k","Ë†","A","a","E","e","D","d","o","/","N","n","J","j","Z","z","i",":",";","X","x","cfâ€˜","câ€˜f","cf}","cf]","cf","c","O{","O","pm","p","C","P]","P","fâ€˜","\"","'","+","f","[","\\","]","}","F","L","M","à¥à¤¾","à¥à¥‹","à¥à¥Œ","à¤…à¥‹","à¤…à¤¾","à¤†à¥ˆ","à¤†à¥‡","à¤¾à¥‹","à¤¾à¥…","à¤¾à¥‡","à¤‚à¥","à¥‡à¥‡","à¤…à¥ˆ","à¤¾à¥‡","à¤…à¥‡","à¤‚à¤¾","à¤…à¥…","à¤¾à¥ˆ","à¥ˆà¤¾","à¤‚à¥ƒ","à¤à¤¾","à¤à¥‚","à¥‡à¤¾","à¤‚à¥‡")
-var array_two=new Array("à¥","à¤½","à¥¤","m'","m]","mfF","mF","à¥¦","à¥§","à¥¨","à¥©","à¥ª","à¥«","à¥¬","à¥­","à¥®","à¥¯","à¤«à¥à¤°","à¤","à¤«","à¤•à¥à¤¤","à¤•à¥à¤°","à¤²","à¤œà¥à¤žà¥","à¤¦à¥à¤˜","à¤œà¥à¤ž","à¤¦à¥à¤¦","à¤¦à¥à¤§","à¤¶à¥à¤°","à¤°à¥","à¤¦à¥à¤¯","à¤•à¥à¤·à¥","à¤¤à¥à¤¤","à¤¦à¥à¤®","à¤¤à¥à¤°","à¤§à¥à¤°","à¤™à¥à¤˜","à¤¡à¥à¤¡","à¤¦à¥à¤°","à¤Ÿà¥à¤Ÿ","à¤¡à¥à¤¢","à¤ à¥à¤ ","à¤°à¥‚","à¤¹à¥ƒ","à¤™à¥à¤—","à¤¤à¥à¤°","à¤™à¥à¤•","à¤™à¥à¤–","à¤Ÿà¥à¤ ","à¤¦à¥à¤µ","à¤Ÿà¥à¤°","à¤ à¥à¤°","à¤¡à¥à¤°","à¤¢à¥à¤°","à¥à¤¯","à¥à¤°","à¤¡à¤¼","à¤¢à¤¼","à¤•à¥","à¤•","à¤–à¥","à¤–","à¤—à¥","à¤—","à¤˜à¥","à¤˜","à¤™","à¤šà¥","à¤š","à¤›","à¤œà¥","à¤œ","à¤à¥","à¤","à¤žà¥","à¤ž","à¤Ÿ","à¤ ","à¤¡","à¤¢","à¤£à¥","à¤¤à¥","à¤¤","à¤¥à¥","à¤¥","à¤¦","à¤§à¥","à¤§","à¤¨à¥","à¤¨","à¤ªà¥","à¤ª","à¤«à¥","à¤¬à¥","à¤¬","à¤­à¥","à¤­","à¤®à¥","à¤®","à¤¯","à¤°","à¤²à¥","à¤²","à¤µà¥","à¤µ","à¤¶à¥","à¤¶","à¤·à¥","à¤¸à¥","à¤¸","à¤¹à¥","à¤¹","à¤‘","à¤‘","à¤”","à¤“","à¤†","à¤…","à¤ˆ","à¤‡","à¤Š","à¤‰","à¤‹","à¤","à¤","à¥‰","à¥‚","à¥","à¤‚","à¤¾","à¥ƒ","à¥","à¥‡","à¥ˆ","à¤","à¥€","à¤ƒ","","à¥‡","à¥ˆ","à¤“","à¤†","à¤”","à¤“","à¥‹","à¥‰","à¥‹","à¥à¤‚","à¥‡","à¤…â€à¥ˆ","à¥‹","à¤…â€à¥‡","à¤¾à¤‚","à¤…â€à¥…","à¥Œ","à¥Œ","à¥ƒà¤‚","à¤¾à¤","à¥‚à¤","à¥‹","à¥‡à¤‚")
+{var array_one=new Array("ç","˜",".","'m","]m","Fmf","Fm",")","!","@","#","$","%","^","&","*","(","k|m","em","km","Qm","qm","N˜","¡","¢","1","2","4",">","?","B","I","Q","ß","q","„","‹","•","›","§","°","¶","¿","Å","Ë","Ì","Í","Î","Ý","å","6«","7«","8«","9«","Ø","|","8Þ","9Þ","S","s","V","v","U","u","£","3","ª","R","r","5","H","h","‰","´","~","`","6","7","8","9","0","T","t","Y","y","b","W","w","G","g","K","k","ˆ","A","a","E","e","D","d","o","/","N","n","J","j","Z","z","i",":",";","X","x","cf‘","c‘f","cf}","cf]","cf","c","O{","O","pm","p","C","P]","P","f‘","\"","'","+","f","[","\\","]","}","F","L","M","्ा","्ो","्ौ","अो","अा","आै","आे","ाो","ाॅ","ाे","ंु","ेे","अै","ाे","अे","ंा","अॅ","ाै","ैा","ंृ","ँा","ँू","ेा","ंे")
+var array_two=new Array("ॐ","ऽ","।","m'","m]","mfF","mF","०","१","२","३","४","५","६","७","८","९","फ्र","झ","फ","क्त","क्र","ल","ज्ञ्","द्घ","ज्ञ","द्द","द्ध","श्र","रु","द्य","क्ष्","त्त","द्म","त्र","ध्र","ङ्घ","ड्ड","द्र","ट्ट","ड्ढ","ठ्ठ","रू","हृ","ङ्ग","त्र","ङ्क","ङ्ख","ट्ठ","द्व","ट्र","ठ्र","ड्र","ढ्र","्य","्र","ड़","ढ़","क्","क","ख्","ख","ग्","ग","घ्","घ","ङ","च्","च","छ","ज्","ज","झ्","झ","ञ्","ञ","ट","ठ","ड","ढ","ण्","त्","त","थ्","थ","द","ध्","ध","न्","न","प्","प","फ्","ब्","ब","भ्","भ","म्","म","य","र","ल्","ल","व्","व","श्","श","ष्","स्","स","ह्","ह","ऑ","ऑ","औ","ओ","आ","अ","ई","इ","ऊ","उ","ऋ","ऐ","ए","ॉ","ू","ु","ं","ा","ृ","्","े","ै","ँ","ी","ः","","े","ै","ओ","आ","औ","ओ","ो","ॉ","ो","ुं","े","अ‍ै","ो","अ‍े","ां","अ‍ॅ","ौ","ौ","ृं","ाँ","ूँ","ो","ें")
 var array_one_length=array_one.length;var modified_substring=document.getElementById("preetiTextArea").value;Replace_Symbols();processed_text=modified_substring;document.getElementById("unicodeTextArea").value=processed_text;function Replace_Symbols()
 {if(modified_substring!="")
 {for(input_symbol_idx=0;input_symbol_idx<array_one_length;input_symbol_idx++)
@@ -11,25 +11,25 @@ var position_of_i=modified_substring.indexOf("l")
 while(position_of_i!=-1)
 {var charecter_next_to_i=modified_substring.charAt(position_of_i+1)
 var charecter_to_be_replaced="l"+charecter_next_to_i
-modified_substring=modified_substring.replace(charecter_to_be_replaced,charecter_next_to_i+"à¤¿")
+modified_substring=modified_substring.replace(charecter_to_be_replaced,charecter_next_to_i+"ि")
 position_of_i=modified_substring.search(/l/,position_of_i+1)}
-var position_of_wrong_ee=modified_substring.indexOf("à¤¿à¥")
+var position_of_wrong_ee=modified_substring.indexOf("ि्")
 while(position_of_wrong_ee!=-1)
 {var consonent_next_to_wrong_ee=modified_substring.charAt(position_of_wrong_ee+2)
-var charecter_to_be_replaced="à¤¿à¥"+consonent_next_to_wrong_ee
-modified_substring=modified_substring.replace(charecter_to_be_replaced,"à¥"+consonent_next_to_wrong_ee+"à¤¿")
-position_of_wrong_ee=modified_substring.search(/à¤¿à¥/,position_of_wrong_ee+2)}
-var position_of_wrong_ee=modified_substring.indexOf("à¤¿à¤‚à¥")
+var charecter_to_be_replaced="ि्"+consonent_next_to_wrong_ee
+modified_substring=modified_substring.replace(charecter_to_be_replaced,"्"+consonent_next_to_wrong_ee+"ि")
+position_of_wrong_ee=modified_substring.search(/ि्/,position_of_wrong_ee+2)}
+var position_of_wrong_ee=modified_substring.indexOf("िं्")
 while(position_of_wrong_ee!=-1)
 {var consonent_next_to_wrong_ee=modified_substring.charAt(position_of_wrong_ee+3)
-var charecter_to_be_replaced="à¤¿à¤‚à¥"+consonent_next_to_wrong_ee
-modified_substring=modified_substring.replace(charecter_to_be_replaced,"à¥"+consonent_next_to_wrong_ee+"à¤¿à¤‚")
-position_of_wrong_ee=modified_substring.search(/à¤¿à¤‚à¥/,position_of_wrong_ee+3)}
-set_of_matras="à¤¾ à¤¿ à¥€ à¥ à¥‚ à¥ƒ à¥‡ à¥ˆ à¥‹ à¥Œ à¤‚ : à¤ à¥…"
+var charecter_to_be_replaced="िं्"+consonent_next_to_wrong_ee
+modified_substring=modified_substring.replace(charecter_to_be_replaced,"्"+consonent_next_to_wrong_ee+"िं")
+position_of_wrong_ee=modified_substring.search(/िं्/,position_of_wrong_ee+3)}
+set_of_matras="ा ि ी ु ू ृ े ै ो ौ ं : ँ ॅ"
 var position_of_R=modified_substring.indexOf("{")
 while(position_of_R>0)
 {probable_position_of_half_r=position_of_R-1;var charecter_at_probable_position_of_half_r=modified_substring.charAt(probable_position_of_half_r)
 while(set_of_matras.match(charecter_at_probable_position_of_half_r)!=null)
 {probable_position_of_half_r=probable_position_of_half_r-1;charecter_at_probable_position_of_half_r=modified_substring.charAt(probable_position_of_half_r);}
-charecter_to_be_replaced=modified_substring.substr(probable_position_of_half_r,(position_of_R-probable_position_of_half_r));new_replacement_string="à¤°à¥"+charecter_to_be_replaced;charecter_to_be_replaced=charecter_to_be_replaced+"{";modified_substring=modified_substring.replace(charecter_to_be_replaced,new_replacement_string);position_of_R=modified_substring.indexOf("{");}
-modified_substring=modified_substring.replace(/=/g,".");modified_substring=modified_substring.replace(/_/g,")");modified_substring=modified_substring.replace(/Ã–/g,"=");modified_substring=modified_substring.replace(/Ã™/g,";");modified_substring=modified_substring.replace(/â€¦/g,"â€˜");modified_substring=modified_substring.replace(/Ãš/g,"â€™");modified_substring=modified_substring.replace(/Ã›/g,"!");modified_substring=modified_substring.replace(/Ãœ/g,"%");modified_substring=modified_substring.replace(/Ã¦/g,"â€œ");modified_substring=modified_substring.replace(/Ã†/g,"â€");modified_substring=modified_substring.replace(/Â±/g,"+");modified_substring=modified_substring.replace(/-/g,"(");modified_substring=modified_substring.replace(/</g,"?");}}}
+charecter_to_be_replaced=modified_substring.substr(probable_position_of_half_r,(position_of_R-probable_position_of_half_r));new_replacement_string="र्"+charecter_to_be_replaced;charecter_to_be_replaced=charecter_to_be_replaced+"{";modified_substring=modified_substring.replace(charecter_to_be_replaced,new_replacement_string);position_of_R=modified_substring.indexOf("{");}
+modified_substring=modified_substring.replace(/=/g,".");modified_substring=modified_substring.replace(/_/g,")");modified_substring=modified_substring.replace(/Ö/g,"=");modified_substring=modified_substring.replace(/Ù/g,";");modified_substring=modified_substring.replace(/…/g,"‘");modified_substring=modified_substring.replace(/Ú/g,"’");modified_substring=modified_substring.replace(/Û/g,"!");modified_substring=modified_substring.replace(/Ü/g,"%");modified_substring=modified_substring.replace(/æ/g,"“");modified_substring=modified_substring.replace(/Æ/g,"”");modified_substring=modified_substring.replace(/±/g,"+");modified_substring=modified_substring.replace(/-/g,"(");modified_substring=modified_substring.replace(/</g,"?");}}}
