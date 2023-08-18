5e9e72011d0422c56b21441a019f949276c34198
janardanaryal <125633317+janardanaryal@users.noreply.github.com>
2023-08-18 15:13:26 -0700

    Update copy-to-clipboard.js

--- assets/js/copy-to-clipboard.js
+++ assets/js/copy-to-clipboard.js
@@ -1,31 +1,39 @@
-document.addEventListener('DOMContentLoaded', function () {
+$(document).ready(function() {
     function copyTextToClipboard(buttonId, outputId) {
-        var copyButton = document.getElementById(buttonId);
+        var copyButton = $('#' + buttonId);
 
-        copyButton.addEventListener('click', function () {
+        copyButton.on('click', function () {
             // Get the textarea element by its ID
-            var textarea = document.getElementById(outputId);
+            var textarea = $('#' + outputId);
 
             // Check if the textarea has content
-            if (textarea.value.trim() === '') {
+            if ($.trim(textarea.val()) === '') {
                 console.log('Textarea is empty. Nothing to copy.');
                 return; // Exit the function
             }
 
-            // Select the text inside the textarea
-            textarea.select();
+            try {
+                // Create a temporary textarea element
+                var tempTextarea = document.createElement('textarea');
+                tempTextarea.value = textarea.val();
+                document.body.appendChild(tempTextarea);
 
-            // Execute the copy command
-            var successful = document.execCommand('copy');
+                // Select the text inside the temporary textarea
+                tempTextarea.select();
 
-            // Display a message indicating success or failure
-            if (successful) {
+                // Use the Clipboard API to copy the text
+                document.execCommand('copy');
+
+                // Remove the temporary textarea from the DOM
+                document.body.removeChild(tempTextarea);
+
                 console.log('Text copied to clipboard.');
-            } else {
-                console.log('Unable to copy text to clipboard.');
+            } catch (err) {
+                console.error('Unable to copy text to clipboard: ' + err);
             }
         });
     }
+
     // Attach event listeners for each button
     copyTextToClipboard('copy-button-unicode', 'unicodeTextArea');
     copyTextToClipboard('copy-button-roman', 'roman-output');
